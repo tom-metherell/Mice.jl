e751e06b04425c613ee203ebf1cc6ae7f2ac6312
Tom Metherell <102831716+tom-metherell@users.noreply.github.com>
2023-07-21 16:02:15 +0100

    Development

--- src/Mice.jl
+++ src/Mice.jl
@@ -10,17 +10,17 @@ module Mice
 """
     mice(data[, m, visitSequence, methods, predictorMatrix, iter])
 
-Impute missing values in a dataset using the MICE algorithm.
+Imputes missing values in a dataset using the MICE algorithm. Heavily based on the R package `mice` (Buuren & Groothuis-Oudshoorn, 2011).
 
 Currently, only predictive mean matching (pmm) is supported. All variables in the dataset will be imputed unless the `method` for that variable is specified as an empty string ("").
 
 # Arguments
 - `data`: The dataset to impute missing values in. Must be supplied as a `DataFrame`.
-- m: The number of imputations to generate. Default is 5.
-- visitSequence: Vector of variables names from the dataset in the order in which they should be imputed. Default is the order in which they appear in the dataset.
-- methods: Vector of imputation methods to use for each variable (in the order specified by `visitSequence`). Default is "pmm" for all variables. "" will skip imputation for that variable.
-- predictorMatrix: Boolean matrix indicating which variables should be used as predictors for each variable (in the order specified by `visitSequence`). Default is to use all other variables as predictors for each variable.
-- iter: Number of iterations. Default is 10.
+- `m`: The number of imputations to generate. Default is 5.
+- `visitSequence`: Vector of variables names from the dataset in the order in which they should be imputed. Default is the order in which they appear in the dataset.
+- `methods`: Vector of imputation methods to use for each variable (in the order specified by `visitSequence`). Default is `pmm` for all variables. "" will skip imputation for that variable.
+- `predictorMatrix`: Boolean matrix indicating which variables should be used as predictors for each variable (in the order specified by `visitSequence`). Default is to use all other variables as predictors for each variable.
+- `iter`: Number of iterations. Default is 10.
 """
     function mice(
         data::DataFrame, 
