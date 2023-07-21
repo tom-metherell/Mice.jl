85abed76d3a1ee87365f53af5af7990e0930fa15
Tom Metherell <102831716+tom-metherell@users.noreply.github.com>
2023-07-21 16:55:51 +0100

    Development

--- src/Mice.jl
+++ src/Mice.jl
@@ -7,17 +7,32 @@ module Mice
     include("micehelperfunctions.jl")
 
 """
-    mice(data::DataFrame, m::Int = 5, visitSequence::Vector{String} = names(data), methods::Vector{String} = nothing, predictorMatrix::Matrix{Bool} = nothing, iter::Int = 10)
+    mice(
+        data::DataFrame, 
+        m::Int = 5, 
+        visitSequence::Vector{String} = names(data), 
+        methods::Vector{String} = nothing, 
+        predictorMatrix::Matrix{Bool} = nothing, 
+        iter::Int = 10
+    )
 
-Imputes missing values in a dataset using the MICE algorithm. Heavily based on the R package `mice` (Buuren & Groothuis-Oudshoorn, 2011).
+Imputes missing values in a dataset using the MICE algorithm. 
+Heavily based on the R package `mice` (Buuren & Groothuis-Oudshoorn, 2011).
 
 The number of imputations created is specified by `m`.
 
-The variables will be imputed in the order specified by `visitSequence`. The default is the order in which they appear in the dataset; the order can be customised by instead using a vector of variable names in the desired order.
+The variables will be imputed in the order specified by `visitSequence`. 
+The default is the order in which they appear in the dataset; 
+the order can be customised using a vector of variable names in the desired order.
 
-The imputation method for each variable is specified by `methods`. The default is to use predictive mean matching (`pmm`) for all variables. Currently only `pmm` is supported. Any variable which is not to be imputed can be marked as such using an empty string (""). 
+The imputation method for each variable is specified by `methods`. 
+The default is to use predictive mean matching (`pmm`) for all variables. 
+Currently only `pmm` is supported. 
+Any variable not to be imputed can be marked as such using an empty string ("").
 
-The predictor matrix is specified by `predictorMatrix`. The default is to use all other variables as predictors for each variable. Any variable which is not to be used as a predictor for another variable can be marked as such in the matrix using a 0.
+The predictor matrix is specified by `predictorMatrix`. 
+The default is to use all other variables as predictors for each variable. 
+Any variable not predicting another variable can be marked as such in the matrix using a 0.
 
 The number of iterations is specified by `iter`.
 """
